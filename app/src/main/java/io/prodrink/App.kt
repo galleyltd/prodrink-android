8dfcecf3a415da90084992421b497de64e2c10e5
Konstantin Chuvilyov <f1nzer@outlook.com>
2017-08-29 20:13:30 +0700

    new dependencies

---
+++ app/src/main/java/io/prodrink/App.kt
@@ -0,0 +1,15 @@
+package io.prodrink
+
+import android.app.Application
+import android.content.Context
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.KodeinAware
+import com.github.salomonbrys.kodein.bind
+import com.github.salomonbrys.kodein.instance
+
+
+class App : Application(), KodeinAware {
+    override val kodein: Kodein = Kodein {
+        bind<Context>() with instance(this@App)
+    }
+}
--- app/src/main/java/io/prodrink/shared/concrete/ActivityScope.kt
+++
@@ -1,6 +0,0 @@
-package io.prodrink.shared.concrete
-
-import javax.inject.Scope
-
-@Scope
-annotation class ActivityScope
