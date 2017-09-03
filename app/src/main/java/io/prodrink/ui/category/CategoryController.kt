150e9328e587cd2968f28affde6c0cff39e7f550
Konstantin Chuvilyov <f1nzer@outlook.com>
2017-09-03 14:37:42 +0700

    changed gradle build, attemp to add travis

--- app/src/main/java/io/prodrink/ui/category/CategoryController.kt
+++ app/src/main/java/io/prodrink/ui/category/CategoryController.kt
@@ -1,17 +1,13 @@
 package io.prodrink.ui.category
 
 import android.os.Bundle
-import android.support.v7.view.ActionMode
 import android.support.v7.widget.LinearLayoutManager
 import android.view.*
 import io.prodrink.R
 import io.prodrink.ui.base.controller.NucleusController
 import kotlinx.android.synthetic.main.categories_controller.view.*
 
-class CategoryController : NucleusController<CategoryPresenter>(),
-        ActionMode.Callback {
-
-    private var actionMode: ActionMode? = null
+class CategoryController : NucleusController<CategoryPresenter>() {
 
     override fun createPresenter() = CategoryPresenter()
 
@@ -34,22 +30,5 @@ class CategoryController : NucleusController<CategoryPresenter>(),
 
     override fun onDestroyView(view: View) {
         super.onDestroyView(view)
-        actionMode = null
-    }
-
-    override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
-        return true
-    }
-
-    override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
-        return true
-    }
-
-    override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
-        return true
-    }
-
-    override fun onDestroyActionMode(mode: ActionMode?) {
-        actionMode = null
     }
 }
