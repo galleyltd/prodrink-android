ac345ba64c2c847470cc2e5ef744f07dcc4eced8
Konstantin Chuvilyov <f1nzer@outlook.com>
2017-09-03 16:30:51 +0700

    added crashlytics, added beta build and readme badges

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # prodrink-android
+[![Build Status](https://travis-ci.org/prodrink/prodrink-android.svg?branch=master)](https://travis-ci.org/prodrink/prodrink-android)
+[![CodeFactor](https://www.codefactor.io/repository/github/prodrink/prodrink-android/badge)](https://www.codefactor.io/repository/github/prodrink/prodrink-android)
--- app/src/main/java/io/prodrink/App.kt
+++ app/src/main/java/io/prodrink/App.kt
@@ -2,16 +2,26 @@ package io.prodrink
 
 import android.app.Application
 import android.content.Context
+import com.crashlytics.android.Crashlytics
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.KodeinAware
 import com.github.salomonbrys.kodein.bind
 import com.github.salomonbrys.kodein.instance
-
+import io.fabric.sdk.android.Fabric
 
 class App : Application(), KodeinAware {
     override val kodein: Kodein = Kodein {
         bind<Context>() with instance(this@App)
 //        import(NetworkModule.module)
 //        import(PresentationModule.module)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        @Suppress("ConstantConditionIf")
+        if (BuildConfig.USE_CRASHLYTICS) {
+            Fabric.with(this, Crashlytics())
+        }
     }
 }
