482e3f38d73b252d11b9937907325cc68fd61946
Konstantin Chuvilyov <f1nzer@outlook.com>
2017-09-04 12:45:26 +0700

    added barcode detection activity

--- app/src/main/java/io/prodrink/ui/category/CategoryController.kt
+++ app/src/main/java/io/prodrink/ui/category/CategoryController.kt
@@ -1,11 +1,14 @@
 package io.prodrink.ui.category
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.*
+import com.jakewharton.rxbinding2.view.clicks
 import io.prodrink.R
 import io.prodrink.ui.base.controller.NucleusController
 import kotlinx.android.synthetic.main.categories_controller.view.*
+import io.prodrink.ui.barcode.BarcodeCaptureActivity
 
 class CategoryController : NucleusController<CategoryPresenter>() {
 
@@ -25,6 +28,13 @@ class CategoryController : NucleusController<CategoryPresenter>() {
         with(view) {
             recycler.layoutManager = LinearLayoutManager(context)
             recycler.setHasFixedSize(true)
+
+            fab.clicks().subscribe({
+                val intent = Intent(context, BarcodeCaptureActivity::class.java).apply {
+                    putExtra(BarcodeCaptureActivity.AutoFocus, true)
+                }
+                startActivity(intent)
+            })
         }
     }
 
